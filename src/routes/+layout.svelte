c5e6b2335151186a09100ad9d17e78c424a1f274
Henry He <hhe@mailfence.com>
2023-02-24 05:28:18 -0500

    basic navbar setup

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+	import { Icon, Label } from '@smui/common';
+	import Button from '@smui/button/src/Button.svelte';
+  import Textfield from '@smui/textfield';
+	import TopAppBar, { Row, Section } from '@smui/top-app-bar';
+	import IconButton from '@smui/icon-button';
+  let search = '';
+</script>
+
+<TopAppBar variant="static" color="primary">
+	<Row>
+
+		<Section>
+			<a href="/"><img class="min-w-[80px]" src="./images/pix-logo.svg" alt="Pix logo" /></a>
+		</Section>
+
+		<Section align="end" toolbar>
+			<IconButton style="margin-right: 1rem" class="material-icons" aria-label="Checkout Items">
+        shopping_bag
+      </IconButton>
+			<Button variant="unelevated">
+				<Label>Sign In</Label>
+			</Button>
+		</Section>
+	</Row>
+</TopAppBar>
+<slot />
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,10 @@
-<script lang="ts"></script>
+<script lang="ts">
+	import Button from '@smui/button';
+</script>
 
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<Button color="primary">RED</Button>
+
+<style lang="scss">
+</style>
